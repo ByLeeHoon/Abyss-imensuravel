f84b3218a8cbcf1c765d90bed7f242858d638781
Kurb koor <rgjosevictor02@gmail.com>
2024-11-13 12:59:56 -0300

    Update scripts-index.js

--- scripts-index.js
+++ scripts-index.js
@@ -2,6 +2,8 @@ document.querySelectorAll('.dadoIcon').forEach(icon => {
   icon.addEventListener('click', function() {
     const resultado = Math.floor(Math.random() * 20) + 1; // Dado de 20 faces
     const pericia = this.dataset.pericia;
+    const bonus = parseInt(document.getElementById(`bonus-${pericia}`).textContent) || 0;
+    const resultadoFinal = resultado + bonus;
     
     fetch('https://discord.com/api/webhooks/1285426560404291687/Pkb3fAR0LOosvxzVyn4PR6oIX20ptqxLYEvqneWjQe_WWR0-4lo-H916jaRAXEVKD-5l', {
       method: 'POST',
@@ -9,11 +11,11 @@ document.querySelectorAll('.dadoIcon').forEach(icon => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        content: `Resultado do dado para ${pericia}: ${resultado}`
+        content: `Resultado do dado para ${pericia}: ${resultadoFinal} (1d20: ${resultado} + Bônus: ${bonus})`
       })
     }).then(response => {
       if (response.ok) {
-        alert(`Resultado enviado para o Discord: ${resultado}`);
+        alert(`Resultado enviado para o Discord: ${resultadoFinal}`);
       } else {
         alert('Falha ao enviar o resultado para o Discord.');
       }
