5e5596b4533673cec0dd8fcb8103969de6fb336e
Kurb koor <rgjosevictor02@gmail.com>
2024-11-13 03:29:26 -0300

    Update and rename scripts.js to scripts-index.js

--- scripts-index.js
+++ scripts-index.js
@@ -2,38 +2,18 @@ const SUPABASE_URL = 'https://hduqbajuilragjnhjgvn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdXFiYWp1aWxyYWdqbmhqZ3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NzUyMDYsImV4cCI6MjA0NzA1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.creation-section');
-    sections.forEach(section => {
-        section.style.display = 'none';
+document.getElementById('search-bar').addEventListener('input', function() {
+    const query = document.getElementById('search-bar').value.toLowerCase();
+    const characterCards = document.querySelectorAll('.character-card');
+    characterCards.forEach(card => {
+        const characterName = card.querySelector('span').textContent.toLowerCase();
+        if (characterName.includes(query)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
     });
-    document.getElementById(sectionId).style.display = 'block';
-}
-
-document.getElementById('save-character-btn').addEventListener('click', async function() {
-    const agi = document.getElementById('agi').value;
-    const forca = document.getElementById('for').value;
-    const intelecto = document.getElementById('int').value;
-    const presenca = document.getElementById('pre').value;
-    const vigor = document.getElementById('vig').value;
-
-    const { data, error } = await supabase
-        .from('characters')
-        .insert([{ agi, forca, intelecto, presenca, vigor }]);
-
-    if (error) {
-        console.error('Erro ao salvar ficha: ', error);
-    } else {
-        alert('Ficha do personagem foi salva na nuvem!');
-        window.location.href = 'index.html';
-    }
 });
-
-window.onload = function() {
-    if (document.getElementById('saved-characters')) {
-        displaySavedCharacters();
-    }
-};
 
 async function displaySavedCharacters() {
     const { data, error } = await supabase.from('characters').select('*');
@@ -59,10 +39,7 @@ function accessCharacter(characterId) {
 }
 
 async function deleteCharacter(characterId) {
-    const { error } = await supabase
-        .from('characters')
-        .delete()
-        .eq('id', characterId);
+    const { error } = await supabase.from('characters').delete().eq('id', characterId);
 
     if (error) {
         console.error('Erro ao excluir ficha: ', error);
@@ -71,3 +48,5 @@ async function deleteCharacter(characterId) {
         displaySavedCharacters();
     }
 }
+
+window.onload = displaySavedCharacters;
