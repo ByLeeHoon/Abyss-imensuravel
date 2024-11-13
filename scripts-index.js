b5912fa3d7b9f9d3e7dbe6edbe9a81f899f852fc
Kurb koor <rgjosevictor02@gmail.com>
2024-11-13 03:41:11 -0300

    Update scripts-index.js

--- scripts-index.js
+++ scripts-index.js
@@ -2,22 +2,12 @@ const SUPABASE_URL = 'https://hduqbajuilragjnhjgvn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdXFiYWp1aWxyYWdqbmhqZ3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NzUyMDYsImV4cCI6MjA0NzA1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-document.getElementById('search-bar').addEventListener('input', function() {
-    const query = document.getElementById('search-bar').value.toLowerCase();
-    const characterCards = document.querySelectorAll('.character-card');
-    characterCards.forEach(card => {
-        const characterName = card.querySelector('span').textContent.toLowerCase();
-        if (characterName.includes(query)) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
-        }
-    });
-});
+window.onload = async function() {
+    await displaySavedCharacters();
+};
 
 async function displaySavedCharacters() {
     const { data, error } = await supabase.from('characters').select('*');
-
     if (error) {
         console.error('Erro ao recuperar fichas: ', error);
     } else {
@@ -26,27 +16,4 @@ async function displaySavedCharacters() {
         data.forEach(character => {
             savedCharactersDiv.innerHTML += `
                 <div class="character-card">
-                    <span>AGI: ${character.agi} | FOR: ${character.forca} | INT: ${character.intelecto} | PRE: ${character.presenca} | VIG: ${character.vigor}</span>
-                    <button onclick="accessCharacter('${character.id}')">Acessar Ficha</button>
-                    <button onclick="deleteCharacter('${character.id}')">Excluir Ficha</button>
-                </div>`;
-        });
-    }
-}
-
-function accessCharacter(characterId) {
-    alert(`Acessando ficha do personagem com ID: ${characterId}`);
-}
-
-async function deleteCharacter(characterId) {
-    const { error } = await supabase.from('characters').delete().eq('id', characterId);
-
-    if (error) {
-        console.error('Erro ao excluir ficha: ', error);
-    } else {
-        alert('Ficha do personagem foi excluída!');
-        displaySavedCharacters();
-    }
-}
-
-window.onload = displaySavedCharacters;
+                    <span>AGI: ${character.agi} | FOR: ${character.forca} | INT: ${character.intelecto} | PRE: ${character.presenca} | VIG: ${character[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/NatanaelSaymon/code-drops/tree/ada5a46222082116f8d070796ed15e4c0539d38b/TEMPLATE_BOOTSTRAP.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/lgfranco22/blog/tree/2ff765f5547038ea91aa40671858d9fd9d5ffb28/entrar.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")
