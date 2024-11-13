c77ee47e2dcec36d5ed225996731049724662eed
Kurb koor <rgjosevictor02@gmail.com>
2024-11-13 03:06:59 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 const SUPABASE_URL = 'https://hduqbajuilragjnhjgvn.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdXFiYWp1aWxyYWdqbmhqZ3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NzUyMDYsImV4cCI6MjA0NzA1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdXFiYWp1aWxyYWdqbmhqZ3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NzUyMDYsImV4cCI6MjA0NzA1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 document.getElementById('new-character-btn').addEventListener('click', function() {
+    console.log('Botão "Nova Ficha" clicado');
     document.getElementById('character-creation').style.display = 'block';
     showSection('attributes');
 });
 
 document.getElementById('save-character-btn').addEventListener('click', async function() {
+    console.log('Botão "Salvar Ficha" clicado');
     const agi = document.getElementById('agi').value;
     const forca = document.getElementById('for').value;
     const intelecto = document.getElementById('int').value;
@@ -28,18 +30,7 @@ document.getElementById('save-character-btn').addEventListener('click', async fu
 });
 
 function showSection(sectionId) {
+    console.log(`Mostrando seção: ${sectionId}`);
     const sections = document.querySelectorAll('.creation-section');
     sections.forEach(section => {
-        section.style.display = 'none';
-    });
-    document.getElementById(sectionId).style.display = 'block';
-}
-
-async function displaySavedCharacters() {
-    const { data, error } = await supabase.from('characters').select('*');
-    
-    if (error) {
-        console.error('Erro ao recuperar fichas: ', error);
-    } else {
-        const savedCharactersDiv = document.getElementById('saved-characters');
-        savedCharactersDiv.innerHTML =[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/NatanaelSaymon/code-drops/tree/ada5a46222082116f8d070796ed15e4c0539d38b/TEMPLATE_BOOTSTRAP.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/lgfranco22/blog/tree/2ff765f5547038ea91aa40671858d9fd9d5ffb28/entrar.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")
+        section.style.display = 'none[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/NatanaelSaymon/code-drops/tree/ada5a46222082116f8d070796ed15e4c0539d38b/TEMPLATE_BOOTSTRAP.md?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/lgfranco22/blog/tree/2ff765f5547038ea91aa40671858d9fd9d5ffb28/entrar.php?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")
