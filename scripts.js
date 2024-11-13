edf986e67f4f082d4fd06523324b06e567902757
Kurb koor <rgjosevictor02@gmail.com>
2024-11-13 02:46:58 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,34 +2,42 @@ const SUPABASE_URL = 'https://hduqbajuilragjnhjgvn.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdXFiYWp1aWxyYWdqbmhqZ3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE0NzUyMDYsImV4cCI6MjA0NzA1MTIwNn0.2D-cZAiS70YUmiwJVC4rKQEyxq3mwofFfA3B9IOW3CE';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-document.getElementById('rpg-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const characterName = document.getElementById('character-name').value;
+document.getElementById('new-character-btn').addEventListener('click', function() {
+    document.getElementById('character-creation').style.display = 'block';
+});
+
+document.getElementById('save-character-btn').addEventListener('click', async function() {
+    const agi = document.getElementById('agi').value;
+    const forca = document.getElementById('for').value;
+    const intelecto = document.getElementById('int').value;
+    const presenca = document.getElementById('pre').value;
+    const vigor = document.getElementById('vig').value;
 
     const { data, error } = await supabase
         .from('characters')
-        .insert([{ name: characterName }]);
-
+        .insert([{ agi, forca, intelecto, presenca, vigor }]);
+    
     if (error) {
         console.error('Erro ao salvar ficha: ', error);
     } else {
-        alert(`Ficha do personagem ${characterName} foi salva na nuvem!`);
+        alert('Ficha do personagem foi salva na nuvem!');
         displaySavedCharacters();
+        document.getElementById('character-creation').style.display = 'none';
     }
 });
 
 async function displaySavedCharacters() {
     const { data, error } = await supabase.from('characters').select('*');
-
+    
     if (error) {
         console.error('Erro ao recuperar fichas: ', error);
     } else {
         const savedCharactersDiv = document.getElementById('saved-characters');
-        savedCharactersDiv.innerHTML = '<h2>Fichas Salvas:</h2>';
+        savedCharactersDiv.innerHTML = '<h2>Fichas Salvas</h2>';
         data.forEach(character => {
             savedCharactersDiv.innerHTML += `
                 <div class="character-card">
-                    <span>${character.name}</span>
+                    <span>AGI: ${character.agi} | FOR: ${character.forca} | INT: ${character.intelecto} | PRE: ${character.presenca} | VIG: ${character.vigor}</span>
                     <button onclick="accessCharacter('${character.id}')">Acessar Ficha</button>
                 </div>`;
         });
@@ -37,7 +45,6 @@ async function displaySavedCharacters() {
 }
 
 function accessCharacter(characterId) {
-    // Implementar funcionalidade para acessar a ficha do personagem
     alert(`Acessando ficha do personagem com ID: ${characterId}`);
 }
 
