dc32b7bae9efa1da7c755d8a7cb8d8fcf104b48c
Kurb koor <rgjosevictor02@gmail.com>
2024-11-16 05:23:35 -0300

    Update and rename scripts-index.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Seleciona o botão "Nova Ficha"
+const novaFichaBtn = document.getElementById('nova-ficha-btn');
+
+// Adiciona o evento de clique para redirecionar para a página de atributos
+novaFichaBtn.addEventListener('click', () => {
+    window.location.href = 'atributos.html'; // Redireciona para atributos.html
+});
