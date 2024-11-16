bbc94148f741d83b609395155bb5465519d6ce31
Kurb koor <rgjosevictor02@gmail.com>
2024-11-16 08:57:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
-// firebase-config.js
+// Importando o Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Sua configuração do Firebase (copie e cole as informações do seu projeto)
+// Sua configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyB0aiEvb-Eg0Hpa_Cvuj1-jQIbPm2d7zJE",
-    authDomain: "abismo-imensuravel.firebaseapp.com",
-    projectId: "abismo-imensuravel",
-    storageBucket: "abismo-imensuravel.firebasestorage.app",
-    messagingSenderId: "915439292491",
-    appId: "1:915439292491:web:76d4a24bf7e6964aeefdce",
-    measurementId: "G-9ZMNV6Y8K8"
+  apiKey: "AIzaSyB0aiEvb-Eg0Hpa_Cvuj1-jQIbPm2d7zJE",
+  authDomain: "abismo-imensuravel.firebaseapp.com",
+  projectId: "abismo-imensuravel",
+  storageBucket: "abismo-imensuravel.firebasestorage.app",
+  messagingSenderId: "915439292491",
+  appId: "1:915439292491:web:76d4a24bf7e6964aeefdce",
+  measurementId: "G-9ZMNV6Y8K8"
 };
 
 // Inicializando o Firebase
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-// Inicializando a autenticação
-const auth = firebase.auth();
-const provider = new firebase.auth.GoogleAuthProvider();
+export { auth, googleProvider };
