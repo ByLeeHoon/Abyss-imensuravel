a569b625625643437cc3da7693d6c0d9f0843000
Kurb koor <rgjosevictor02@gmail.com>
2024-11-16 13:24:30 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,33 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 // Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB0aiEvb-Eg0Hpa_Cvuj1-jQIbPm2d7zJE",
   authDomain: "abismo-imensuravel.firebaseapp.com",
   projectId: "abismo-imensuravel",
-  storageBucket: "abismo-imensuravel.appspot.com",
+  storageBucket: "abismo-imensuravel.firebasestorage.app",
   messagingSenderId: "915439292491",
   appId: "1:915439292491:web:76d4a24bf7e6964aeefdce",
+  measurementId: "G-9ZMNV6Y8K8"
 };
 
-// Inicializa o Firebase
+// Inicializando o Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider(); // Provider para login do Google
 
-// Função para o login
-document.getElementById("google-login-btn").addEventListener("click", () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log("Usuário logado:", user);
-      localStorage.setItem("usuarioLogado", user.uid); // Salva o ID do usuário
-      window.location.href = "index.html"; // Redireciona para a página inicial
-    })
-    .catch((error) => {
-      console.error("Erro ao fazer login:", error);
-      document.getElementById("error-message").innerText = "Erro ao fazer login.";
-      document.getElementById("error-message").style.display = "block";
-    });
-});
+export { auth, googleProvider };
